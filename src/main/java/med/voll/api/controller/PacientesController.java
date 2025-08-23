2d8850b0f09532a0bf7314747a6518e55ba54eb7
alexandre-ia <alexandrehenriquepereira310@gmail.com>
2025-08-23 09:58:49 -0300

    Inicio do projeto

---
+++ src/main/java/med/voll/api/controller/PacientesController.java
@@ -0,0 +1,20 @@
+package med.voll.api.controller;
+
+
+import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.paciente.DadosCadastroPaciente;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("paciente")
+public class PacientesController {
+
+    @PostMapping
+    public void cadastrarPaciente(@RequestBody DadosCadastroPaciente dadosCadastroPaciente) {
+        System.out.println("Cadastrando Paciente " + dadosCadastroPaciente);
+
+    }
+}
